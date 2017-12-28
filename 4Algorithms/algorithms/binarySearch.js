165e8cfed40f7e5b43857c254163f296129c7f30
Abhijith Simhadri <abhijith142@gmail.com>
2017-12-28 19:48:51 +0530

    Update binarySearch.js

--- 4Algorithms/algorithms/binarySearch.js
+++ 4Algorithms/algorithms/binarySearch.js
@@ -1,7 +1,23 @@
 /*
   A JavaScript module which performs a binary search on an array of numbers ordered low to high.  If the specified value is found its index in the input array is returned.  If the specified value is not found -1 is returned.
 */
-module.exports = exports = function(input, value) {
+module.exports = exports = function binarySearch(input, value) {
   var bottom = 0, top = input.length - 1, index = null;
+  var found = false;
+
+  while(bottom<top){
+    var middle = parseInt((bottom+top)/2);
+    if(value<input[middle]){
+      top = middle-1;
+    }else if(value>input[middle]){
+      bottom = middle +1;
+    }else{
+      found = true;
+      index = middle;
+    }
+  }
+  console.log(index);
+  if(found === true)return index;
+  return -1;
 //  complete the function
 };
