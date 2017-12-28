719ca184a3f59763f72726d04d0c62c1d9c8315e
Abhijith Simhadri <abhijith142@gmail.com>
2017-12-28 19:49:26 +0530

    Update bubbleSort.js

--- 4Algorithms/algorithms/bubbleSort.js
+++ 4Algorithms/algorithms/bubbleSort.js
@@ -3,4 +3,14 @@
 */
 module.exports = exports = function(input) {
   //  complete the function
+  for(var j=1;j<input.length;j++){
+    for(var i=0;i<input.length-j;i++){
+      if(input[i]>input[i+1]){
+        var temp = input[i];
+        input[i]=input[i+1];
+        input[i+1]=temp;
+      }
+    }
+  }
+  return input;
 };
